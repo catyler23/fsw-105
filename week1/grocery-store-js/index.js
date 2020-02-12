176c7aa72bf61bcc1d81e4c0b61682bbe189e9f8
catyler23 <54535221+catyler23@users.noreply.github.com>
2020-02-12 11:11:45 -0500

    javascript practice

---
+++ week1/grocery-store-js/index.js
@@ -0,0 +1,24 @@
+var name = "Justin"; // (Strings)
+var age = 30; // (Numbers)
+var isAlive = true; // (Boolean)
+
+console.log(name.length);
+console.log(isAlive);
+
+//----------------------------------
+
+// var person = {
+//   name: "justin",          // (Object)
+//   age: 30,
+//   isAlive: true
+// };
+
+// console.log(person.isAlive);
+// console.log(person.age);
+
+//-----------------------------------
+
+// var shopper = ["strawberry", "mango", "grapes", "kiwi"];
+
+// console.log(shopper.length);               // (Array)
+// console.log(shopper[0]);
