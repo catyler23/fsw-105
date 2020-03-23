a44f5787454c07f9431c380e1c1530dae3f37125
catyler23 <54535221+catyler23@users.noreply.github.com>
2020-03-23 02:19:01 -0400

    escaperoom'

--- week6/project3:escape/app.js
+++ week6/project3:escape/app.js
@@ -9,30 +9,30 @@ const intro = `Welcome to the Escape Room Stimulation!`;
 console.log(intro);
 
 while (isAlive == true) {
-    const gameMenuOptions = readlineSync.keyIn(`Press the number keys to enter an option \n 1. Put hand in hole \n 2. Find the key, or \n 3. Open the door \n Enter your selection:`, { limit: `$< 1-3>` });
-        console.log(typeof gameMenuOptions)   
-    if (gameMenuOptions == 1) {
+    const menuID = readlineSync.keyIn(`Press the number keys to enter an option \n 1. Put hand in hole \n 2. Find the key, or \n 3. Open the door \n Enter your selection:`, {limit: '$<1-3>'});
 
-        console.log(`Oh no! ${playerName}, it was a trap! You are Dead. Sorry!`);
+    if (menuID == 1) {
+
+        console.log(`Oh no! ${playerName}, you put your hand in the hole and it was a trap! You are Dead. Sorry!`);
         isAlive = false;
 
-    } else if (gameMenuOptions == 2 && hasKey == false) {
+    } else if (menuID == 2 && hasKey == false) {
 
-        console.log(`You found the key successfully..... proceed with the game!`);
+        console.log(`${playerName}, You found the key successfully..... proceed with the game!`);
         hasKey = true;
 
-    } else if (gameMenuOptions == 2 && hasKey == true) {
+    } else if (menuID == 2 && hasKey == true) {
 
-        console.log(`You already found the key earlier... move to the next step!`);
+        console.log(`${playerName}, You already found the key earlier... move to the next step!`);
 
 
-    } else if (gameMenuOptions == 3 && hasKey == false) {
+    } else if (menuID == 3 && hasKey == false) {
 
         console.log(`You can turn the handle of the door... It is locked... find the key first!`);
 
-    } else if (gameMenuOptions == 3 && hasKey == true) {
+    } else if (menuID == 3 && hasKey == true) {
 
-        console.log(`Congratulations!  You have found the key and have successfully unlocked the door!`);
+        console.log(`Congratulations ${playerName}!  You have found the key and have successfully unlocked the door!`);
         isAlive = false;
     }
 };
